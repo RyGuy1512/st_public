ffd41d79fe089122824eb9fab8fe60ba89e01411
RyGuy1512 <153574791+RyGuy1512@users.noreply.github.com>
2024-05-23 18:54:26 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,16 @@
-import streamlit as st
 
-st.write("Hello World")
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+import streamlit
+import pprint
+
+uri = "mongodb+srv://ryanwalter12:IDEEzWF3n5Tz02o0@cluster0.i3rreu4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+# Create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'))
+
+db = client.sample_mflix
+x = db.list_collection_names()
+
+
+st.write(x)
